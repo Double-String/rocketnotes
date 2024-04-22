24c95b89250310bfa45b3fb07039d8c0aea46e7b
dani <moitidani@gmail.com>
2024-04-22 10:48:37 -0500

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -13,7 +13,6 @@ export default function Details(){
       <main>
         <Content>
         <ButtonText title="Delete note"/>
-
         <h1>Title</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui reiciendis quae sit dolor voluptatum odit sequi quas ut voluptatibus necessitatibus beatae dolore odio, ducimus neque, distinctio explicabo ipsum? Debitis!</p>
 
